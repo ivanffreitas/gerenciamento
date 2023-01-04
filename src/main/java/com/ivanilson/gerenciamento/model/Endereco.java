08173d75bd005d63be86ebffade1e36685f552b2
ivanffreitas <ivanff.ti@gmail.com>
2023-01-04 08:58:16 -0300

    Model pessoa e endereco

--- src/main/java/com/ivanilson/gerenciamento/model/Endereco.java
+++ src/main/java/com/ivanilson/gerenciamento/model/Endereco.java
@@ -1,2 +1,37 @@
-package com.ivanilson.gerenciamento.model;public class Endereco {
+package com.ivanilson.gerenciamento.model;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "TB002_ENDERECO")
+public class Endereco {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "idendereco")
+    private Long id;
+
+    @Column(name = "logradouro", nullable = false)
+    private String logradouro;
+
+    @Column(name = "cep", nullable = false)
+    private String cep;
+
+    @Column(name = "numero", nullable = false)
+    private String numero;
+
+    @Column(name = "cidade", nullable = false)
+    private String cidade;
+
+    @ManyToOne
+    @JoinColumn(name = "idpessoa", nullable = false)
+    private Pessoa pessoa;
+
 }
--- src/main/java/com/ivanilson/gerenciamento/model/Pessoa.java
+++ src/main/java/com/ivanilson/gerenciamento/model/Pessoa.java
@@ -1,2 +1,31 @@
-package com.ivanilson.gerenciamento.model;public class Pessoa {
+package com.ivanilson.gerenciamento.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "TB001_PESSOA")
+public class Pessoa {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "idpessoa")
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "nascimento", nullable = false)
+    private LocalDate nascimento;
+
+    @OneToMany(mappedBy = "pessoa")
+    private List<Endereco> enderecos;
+
 }
